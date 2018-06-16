5ebb964f8540552f0b4eff4a4d31a37bceda8147
Elio Rivero <eliorivero@gmail.com>
2018-06-15 21:39:54 -0300

    Update code to be compatible with Gutenberg 3.x

--- fold/block.js
+++ fold/block.js
@@ -1,38 +1,35 @@
 const { __ } = wp.i18n;
 const {
-	registerBlockType,
-	Editable,
-	query: {
-		children
-	}
-} = wp.blocks;
+	RichText
+} = wp.editor;
 const {
 	Button
 } = wp.components;
 import uniqueId from 'lodash/uniqueId';
 
-registerBlockType( 'gutenblocks/fold', {
+wp.blocks.registerBlockType( 'gutenblocks/fold', {
 	title: __( 'Fold' ),
 	icon: 'sort',
 	category: 'layout',
 	attributes: {
-		title: children( 'label' ),
-		reveal: children( 'div.gutenblocks-fold-reveal' ),
+		title: {
+			type: 'string',
+			source: 'children',
+			selector: 'label',
+		},
+		reveal: {
+			type: 'array',
+			source: 'children',
+			selector: 'div.gutenblocks-fold-reveal',
+		},
 	},
 	edit: props => {
-		const focusedEditable = props.focus ? props.focus.editable || 'title' : null;
 		const attributes = props.attributes;
 		const onChangeTitle = value => {
 			props.setAttributes( { title: value } );
 		};
-		const onFocusTitle = focus => {
-			props.setFocus( _.extend( {}, focus, { editable: 'title' } ) );
-		};
 		const onChangeReveal = value => {
 			props.setAttributes( { reveal: value } );
-		};
-		const onFocusReveal = focus => {
-			props.setFocus( _.extend( {}, focus, { editable: 'reveal' } ) );
 		};
 		const addNewFold = e => {
 			props.insertBlocksAfter( [ wp.blocks.createBlock( 'gutenblocks/fold' ) ] );
@@ -40,27 +37,23 @@ registerBlockType( 'gutenblocks/fold', {
 
 		return (
 			<div className={ props.className }>
-				<Editable
+				<RichText
 					tagName="label"
 					multiline={ false }
 					placeholder={ __( 'Write visible text…' ) }
 					value={ attributes.title }
 					onChange={ onChangeTitle }
-					focus={ focusedEditable === 'title' }
-					onFocus={ onFocusTitle }
 					/>
 				{
-					props.focus && (
+					props.isSelected && (
 						<div>
-							<Editable
+							<RichText
 								tagName="div"
 								multiline="p"
 								className="gutenblocks-fold-reveal"
 								placeholder={ __( 'And the text to reveal…' ) }
 								value={ attributes.reveal }
 								onChange={ onChangeReveal }
-								focus={ focusedEditable === 'reveal' }
-								onFocus={ onFocusReveal }
 							/>
 							<Button
 								className="button"
